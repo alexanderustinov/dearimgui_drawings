cafbb0ec063c1f2d6d8dcd22338ac47c8e63672e
Polina Nazarova <ktotochra9@gmail.com>
2023-12-04 00:21:27 +0300

    improved to 12.5 fps

--- luvelayn/src/main.cpp
+++ luvelayn/src/main.cpp
@@ -14,42 +14,41 @@
 #include <SDL_opengl.h>
 #endif
 
-const double epsilon = 1e-6;
-
-// Function to iterate Newton's method
-std::complex<double> newton_iteration(const std::complex<double>& z) {
-    return z - (z*z*z - 1.0) / (3.0 * z*z);
-}
-
 void newton_fractal(ImDrawList* drawlist, int width, int height, int max_iter) {
+    const double epsilon = 1e-6;
+
     const double x_min = -2.0;
     const double x_max = 2.0;
     const double y_min = -2.0;
     const double y_max = 2.0;
 
-    // Reduce the number of vertices by skipping some points
-    const int step = 6;
+    const double x_step = (x_max - x_min) / (width - 1);
+    const double y_step = (y_max - y_min) / (height - 1);
 
-    for (int i = 0; i < width; i += step) {
-        for (int j = 0; j < height; j += step) {
-            double x = x_min + (x_max - x_min) * i / (width - 1);
-            double y = y_min + (y_max - y_min) * j / (height - 1);
+    for (int j = 0; j < height; ++j) {
+        double y = y_min + y_step * j;
+
+        for (int i = 0; i < width; ++i) {
+            double x = x_min + x_step * i;
 
             std::complex<double> z(x, y);
 
             int iter = 0;
-            while (iter < max_iter && std::abs(z * z * z - 1.0) > epsilon) {
-                z = newton_iteration(z);
+            double norm_z_cubed_minus_one = std::norm(z * z * z - 1.0);
+
+            const ImVec2 point(i, height - j);
+            while (iter < max_iter && norm_z_cubed_minus_one > epsilon) {
+                z -= (z * z * z - 1.0) / (3.0 * z * z);
                 ++iter;
+                norm_z_cubed_minus_one = std::norm(z * z * z - 1.0);
             }
 
-            ImVec2 point(i, height - j);
             if (iter == max_iter) {
-                drawlist->AddRectFilled(point, ImVec2(i + step, height - j + step), IM_COL32(0, 0, 0, 255));
+                drawlist->AddRectFilled(point, ImVec2(i + 1, height - j + 1), IM_COL32(0, 0, 0, 255));
             } else {
                 float intensity = static_cast<float>(iter) / static_cast<float>(max_iter);
                 ImVec4 color(intensity, 0.5f, 1.0f - intensity, 1.0f);
-                drawlist->AddRectFilled(point, ImVec2(i + step, height - j + step), ImGui::ColorConvertFloat4ToU32(color));
+                drawlist->AddRectFilled(point, ImVec2(i + 1, height - j + 1), ImGui::ColorConvertFloat4ToU32(color));
             }
         }
     }
@@ -105,7 +104,7 @@ int main(int, char **) {
         SDL_WINDOWPOS_CENTERED, 800, 600, window_flags);
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, gl_context);
-    SDL_GL_SetSwapInterval(1);
+    SDL_GL_SetSwapInterval(0);
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
@@ -121,63 +120,59 @@ int main(int, char **) {
     ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     bool done = false;
-    static float angle = 0;
-    const int length = 100;
-    while (!done) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            ImGui_ImplSDL2_ProcessEvent(&event);
-            if (event.type == SDL_QUIT)
-                done = true;
-            if (event.type == SDL_WINDOWEVENT &&
-                event.window.event == SDL_WINDOWEVENT_CLOSE &&
-                event.window.windowID == SDL_GetWindowID(window))
-                done = true;
-        }
+        while (!done) {
+            SDL_Event event;
+            while (SDL_PollEvent(&event)) {
+                ImGui_ImplSDL2_ProcessEvent(&event);
+                if (event.type == SDL_QUIT)
+                    done = true;
+                if (event.type == SDL_WINDOWEVENT &&
+                    event.window.event == SDL_WINDOWEVENT_CLOSE &&
+                    event.window.windowID == SDL_GetWindowID(window))
+                    done = true;
+            }
 
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplSDL2_NewFrame();
-        ImGui::NewFrame();
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplSDL2_NewFrame();
+            ImGui::NewFrame();
 
-        {
-            static float speed = 0.5f;
-            static int counter = 0;
+            {
+                static int counter = 0;
 
-            ImGuiViewport *viewport = ImGui::GetMainViewport();
-            auto size = viewport->WorkSize;
-            ImGui::SetNextWindowPos(viewport->WorkPos);
-            ImGui::SetNextWindowSize(size);
+                ImGuiViewport *viewport = ImGui::GetMainViewport();
+                auto size = viewport->WorkSize;
+                ImGui::SetNextWindowPos(viewport->WorkPos);
+                ImGui::SetNextWindowSize(size);
 
-            ImGui::Begin(
-                "Hello, world!", NULL,
-                ImGuiWindowFlags_NoDecoration);
+                ImGui::Begin(
+                    "Hello, world!", NULL,
+                    ImGuiWindowFlags_NoDecoration);
 
-            const ImVec2 center{size.x / 2, size.y / 2};
+                const ImVec2 center{size.x / 2, size.y / 2};
 
-            int max_iter = 42;
-            newton_fractal(ImGui::GetWindowDrawList(), size.x, size.y, max_iter);
+                int max_iter = 15;
+                newton_fractal(ImGui::GetWindowDrawList(), size.x, size.y, max_iter);
 
-            counter++;
-            ImGui::Text("counter = %d", counter);
+                counter++;
+                ImGui::Text("counter = %d", counter);
 
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-                        1000.0f / io.Framerate, io.Framerate);
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                            1000.0f / io.Framerate, io.Framerate);
 
-            ImGui::End();
-        }
+                ImGui::End();
+            }
 
-        // Rendering
-        ImGui::Render();
-        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w,
-                    clear_color.z * clear_color.w, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        SDL_GL_SwapWindow(window);
+            // Rendering
+            ImGui::Render();
+            glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+            glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w,
+                        clear_color.z * clear_color.w, clear_color.w);
+            glClear(GL_COLOR_BUFFER_BIT);
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+            SDL_GL_SwapWindow(window);
     }
 
     ImGui_ImplOpenGL3_Shutdown();
